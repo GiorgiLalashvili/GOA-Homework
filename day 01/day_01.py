ee0050ecefb8a18f704997930b668246cd32f064
GiorgiLalashvili <giorgilala7@gmail.com>
2024-04-26 22:34:15 +0400

    second commit

---
+++ day 01/day_01.py
@@ -0,0 +1,22 @@
+# name  არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "giorgi" არის ცვლადის მნიშვნელობა
+
+
+name = "Giorgi"              #str ტიპის ცვლადი 
+surname = "Lalashvili"       #str ტიპის ცვლადი 
+age = 15                     #int ტიპის ცვლადი (მთელი რიცხვი)
+height = 163.5               #float ტიპის ცვლადი (ათწილადი)
+kai_biwi = True              #bool ტიპის ცვლადი
+ 
+
+print("Me var" + " " + name + " " + surname + " " + str(age) + " " + "wlis" + " " + "simagle"+ " " + str(height) + "cm" + " " + str(kai_biwi))
+#print ფუნქციას გადაეცება ეკრანზე გამოსატანი ობიექტი
+
+
+ #print(type(name))
+ #print(type(surname))
+ #print(type(age))
+ #print(type(height))
+ #print(type(kai_biwi))
+
