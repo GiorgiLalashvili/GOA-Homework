675328383fb143e9b49a56f58d889226276f9cac
GiorgiLalashvili <Giorgilala7@gmail.com>
2024-05-19 20:38:20 +0400

    second commit

---
+++ level 3/classwork/classwork.py
@@ -0,0 +1,16 @@
+number1 = 9734
+number2 = 266.69
+print(number1+number2)
+
+# აქ გამოიტანა float ტიპის რიცხვი რადგან int+float= float
+# ესე გამოაქვს პროგრამას რადგან პითონმა არიცის მიღებული რიცხვი მთელი იქნება თუ არა
+
+bool1 = True
+bool2 = True
+print(int(bool1) + int(bool2))
+
+# აქ გამოიტანა int რადგან 0 და 1 ების მიმატებით ათწილადს ვერ მივიღებთ
+
+number3 = 20
+number4 = 24
+print(str(number3)+ str(number4))
