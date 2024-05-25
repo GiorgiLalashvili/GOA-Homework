8f824d7179978392b358fc402746c8f070ff0077
GiorgiLalashvili <Giorgilala7@gmail.com>
2024-05-25 17:03:39 +0400

    second commit

---
+++ level 4/classwork/test.py
@@ -0,0 +1,43 @@
+sentence = "This is giorgi's bike"
+text = "giorgi"
+number = "10"
+print(int(number) + 10)
+num1 = 20
+num2 = 24
+
+sum1 = num1 + num2
+
+print(sum1)
+
+budget = 200
+print(budget)
+x = 200
+y = 50
+print(x + y)
+
+number = 100-1
+print(number)
+
+num=10
+num=2
+print(num)
+num=40
+print(num)
+#reassing - გადაწერა
+
+
+num = 2
+num = num + 1
+print(num)
+#increment - ინკრემენტაცია
+
+num = 3
+num = 4
+print(num)
+#გადაწერისას კომპიუტერი ივიწყებს ძველ მნიშვნელობას და თუ პრინტს გამოვიყენებთ გამოიტანს ბოლოს მინიჭებულ მნიშვნელობას
+
+balance = 10
+price = 6
+discount = price * 0.8
+total = (discount)
+print (total)
