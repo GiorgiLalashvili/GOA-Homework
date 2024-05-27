9b89c0ce4363ee0d64c850063eaa826a2e873935
GiorgiLalashvili <Giorgilala7@gmail.com>
2024-05-27 15:44:12 +0400

    second commit

---
+++ level 5/classwork/classwork.py
@@ -0,0 +1,18 @@
+#reassing არის მნიშვნელობის გადაწერა და ის ცვლადს უცვლის მნიშვნელობას
+number_1 = 5
+number_1 = 6
+print(number_1)
+
+#snake case გამოიყენება pythonshi როგორც space
+#მაგალითად
+TBC_bank_balance = 30
+print(TBC_bank_balance)
+#comments გვეხმარება ადამიანებს კოდის წაკითხვაში და მის შესწორებაშ
+
+#python არის case sensitive ენა ამიტომ უნდა დავაკვირდეთ პატარა და დიდ ასოებს
+#მაგალითად
+
+#  Credit = 400
+#  print(credit)
+#  ეს იქნებოდა ERROR რადგან ასოები არ ემთხვევა 
+
---
+++ level 5/classwork/test.py
@@ -0,0 +1,24 @@
+# case sensitive
+
+Credit = 300
+CREDIT = 200
+CreDit = 100
+credit = 50
+creD1t = 1500
+
+print(Credit)
+print(CREDIT)
+print(CreDit)
+print(credit)
+print(creD1t)
+
+
+#1num = 2000
+#error
+
+# f string
+dog1 = "Bubu"
+dog2 = "Chapi"
+
+sentence = f"first dog's name is {dog1}. second dogs name is {dog2}."
+print(sentence)
