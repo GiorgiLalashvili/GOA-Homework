5b4cf920a65d3194755e0258c99682a57f7b9e06
GiorgiLalashvili <Giorgilala7@gmail.com>
2024-06-04 17:58:43 +0400

    second commit

---
+++ level 6/classwork/test.py
@@ -0,0 +1,31 @@
+num1 = 10
+num2 = 20
+num3 = 30
+num4 = 40
+num5 = 100
+
+print(f"The loading is on {num1} percent...")
+print(f"The loading is on {num2} percent...")
+print(f"The loading is on {num3} percent...")
+print(f"The loading is on {num4} percent...")
+print(f"The loading is on {num5} percent...")
+
+print("the loading is on" + " " + str(num1) + " " + "percent...")
+
+
+# Task 1
+
+#declared variables and gave it names and values
+#used snakecase
+dog_name1 = "Rick"
+dog_name2 = "John"
+dog_name2 = "Max"
+#used print to output f string on display
+print(f"First dog's name is {dog_name1} Second dog's name is {dog_name2}.")
+
+# input
+# Task 2
+
+num1 = int(input("first number: "))
+num2 = int(input("second number: "))
+print((num1) + (num2))
---
+++ level 6/homework/homework.py
@@ -0,0 +1,55 @@
+# Task 1
+#addition
+
+#We told humans instructions
+print("Enter 3 numbers and computer will display addition of them.")
+#We declared 3 variables and gave it value that person types in input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+addition = num1 + num2 + num3
+print(f"Final number is: {addition}")
+
+#multiplication
+
+#We told humans instructions
+print("Enter 3 numbers and computer will display multiplication of them.")
+#We declared 3 variables and gave it value that person types in input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+multiplication = num1 * num2 * num3
+print(f"Final number is: {multiplication}")
+
+#division
+
+#We told humans instructions
+print("Enter 3 numbers and computer will divide them.")
+#We declared 3 variables and gave it value that person types in input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+divide = num1 / num2 / num3
+print(f"Final number is: {divide}")
+
+
+# Task 2
+#create account
+print("Hi to create your account you have to type your information.")
+name = input("Your name: ")
+surname = input("Your surname: ")
+gmail = input("Your gmail:  ")
+password = input("your password: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
