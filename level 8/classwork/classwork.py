a73b2cb9a2081f31f7720e4862aa63c85f4508da
GiorgiLalashvili <Giorgilala7@gmail.com>
2024-06-09 15:33:59 +0400

    second commit

---
+++ level 8/classwork/classwork.py
@@ -0,0 +1,50 @@
+# # string float integer boolean
+# fruit = "apple"
+# price = 3.5
+# amount = 4
+# fruit_is_new = True
+# print(type(fruit))
+# print(type(price))
+# print(type(amount))
+# print(type(fruit_is_new))
+
+# is_right = True
+# is_wrong = False
+# print(is_right + is_wrong)
+
+# #implicit როცა პროგრამისტი გადააკეთებს
+# #explicit როცა პროგრამა თვითონ გადააკეთებს
+
+# print(5 > 3)
+# print(5 < 3)
+# print(type(True))
+# print(5 == 5)
+
+
+
+#Task 1
+
+# 1)
+print(5 > 6) #False
+print(5 < 6) #True
+print(5 == 6) #False
+print()
+# 2)
+print(False and False) #False
+print(True and False) #False
+print(False and True) #False
+print(True and True) #true
+print()
+
+print(False or False) #False
+print(True or False) #True
+print(False or True) #True
+print(True or True) #True
+print()
+# 3)
+print(5 > 2 and 5 > 4) #True
+print(5 > 6 or 5 > 10) #False
+print(False == 0) #True
+print(True > 0) #true
+
+
---
+++ level 8/homework/homework.py
@@ -0,0 +1,50 @@
+#Homework
+
+# Task 1
+print(10 >= 10) #True
+print(10 <= 10) #True
+print(10 <= 9) #False
+print(10 >= 11) #False
+print()
+
+# Task 2
+# != - is not equal
+print(10 != 9) #True
+print(10 != 10) #False
+print(10 != 10 or 10 !=9) #True
+print(50 != 50 or 43 != 43) #False
+print()
+
+# Task 3
+print(3>2 and 5>4) #True
+print(3>5 and 100>1) #False
+print(40>50 and 3>50) #False
+print()
+print(10<11 and 4<5) #True
+print(40<50 and 30<29) #False
+print(40<50 and 30<25) #False
+print()
+print(10<=11 or 4<=5) #True
+print(40<=26 or 31<=29) #False
+print(40<=50 or 30<=30) #True
+print()
+print(10!=10 or 10==10) #True
+print(34==34 and 34!=34) #False
+print(40>=10 or 30<=40) #True
+
+# Task 4
+# and ოპერატორის გამოყენებისას ტერმინალში გამოაქვს falase იმ შემთხვევაში ერთი მაინც თუ არის ის და თუ ყველა true-ა მაშინ გამოაქვს true
+# or ოპერატორი კი ცდილობს რომ true გამოიტანოს და 1 მაინც თუა ის მაშინ true-ს გამოიტანს თუ არა false-ს
+
+# Task 5 
+num1 = input("Enter number: ")
+num2 = input("Enter second number: ")
+print()
+print("num1 is bigger number then num2: ")
+print(num1 > num2)
+print("num1 is bigger number then num2: ")
+print(num1 < num2)
+print("num1 is equal to num2: ")
+print(num1 == num2)
+
+
